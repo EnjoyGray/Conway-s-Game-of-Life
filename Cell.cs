15f41e5a88a7cbfdcad7c0c9443ae0bd27eaa828
EnjoyGray <vanya.konrad@gmail.com>
2024-08-11 10:56:09 +0200

    add Cell class

---
+++ Cell.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conway_s_Game_of_Life
+{
+    internal class Cell
+    {
+        int[] hp = { 0, 1, 2, 3, 4 };
+        string[] visual = { "•", "▫", "▪", "□", "■" };
+
+        int index;
+
+        public int Index
+        {
+            get { return index; }
+            private set
+            {
+                if (value < 0 || value >= hp.Length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Index is out of range.");
+                }
+                index = value;
+            }
+        }
+
+
+        public int Hp
+        {
+            get { return hp[index]; }
+        }
+
+        public string Visual
+        {
+            get { return visual[index]; }
+        }
+
+        public Cell(int Index)
+        {
+            index = Index;
+        }        
+    }
+}
